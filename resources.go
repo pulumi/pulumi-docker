ddea955bd29fd1c1574044605da665d90c7f350d
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-01-31 15:46:58 -0800

    proper deps

--- resources.go
+++ resources.go
@@ -112,7 +112,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
-				"@pulumi/pulumi": "=0.16.15-dev.1548967161",
+				"@pulumi/pulumi": "feature-proxyOutput",
 				"semver":         "^5.4.0",
 			},
 			DevDependencies: map[string]string{
