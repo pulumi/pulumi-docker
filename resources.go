16764e9f163426dd1ae236d8ca2ee83b5bb3f96f
Cyrus Najmabadi <cyrus.najmabadi@gmail.com>
2019-12-17 20:49:49 -0800

    Personal testing

--- resources.go
+++ resources.go
@@ -128,7 +128,7 @@ func Provider() tfbridge.ProviderInfo {
 		},
 		JavaScript: &tfbridge.JavaScriptInfo{
 			Dependencies: map[string]string{
-				"@pulumi/pulumi": "^1.0.0",
+				"@pulumi/pulumi": "feature-dedeasync6",
 				"semver":         "^5.4.0",
 			},
 			DevDependencies: map[string]string{
