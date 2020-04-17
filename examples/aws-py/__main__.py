56d12e609ee8e3c8a3afe877bcac7810b147f4a5
rienafairefr <rienafairefr@gmail.com>
2020-04-17 13:54:52 +0200

    try to force aws-py example to fail reliably

--- examples/aws-py/__main__.py
+++ examples/aws-py/__main__.py
@@ -18,25 +18,28 @@ import pulumi_aws as aws
 import pulumi_docker as docker
 
 # Create a private ECR registry.
+
+
 repo = aws.ecr.Repository('my-repo')
 
+
 # Get registry info (creds and endpoint) so we can build/publish to it.
-def getRegistryInfo(rid):
+def get_registry_info(rid):
     creds = aws.ecr.get_credentials(registry_id=rid)
     decoded = base64.b64decode(creds.authorization_token).decode()
     parts = decoded.split(':')
     if len(parts) != 2:
         raise Exception("Invalid credentials")
     return docker.ImageRegistry(creds.proxy_endpoint, parts[0], parts[1])
-registry = repo.registry_id.apply(getRegistryInfo)
+
+
+registry = repo.registry_id.apply(get_registry_info)
 
 # Build and publish the image.
-image = docker.Image('my-image',
-    build='app',
-    image_name=repo.repository_url,
-    registry=registry,
-)
-
-# Export the resulting base name in addition to the specific version pushed.
-pulumi.export('baseImageName', image.base_image_name)
-pulumi.export('imageName', image.image_name)
+for i in range(10):
+    docker.Image(
+        'my-image-%i' % i,
+        build=docker.DockerBuild(context='app', args={'parameter': str(i)}),
+        image_name=repo.repository_url,
+        registry=registry,
+    )
