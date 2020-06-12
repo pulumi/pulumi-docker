9ea35779a8685ca51f99f9464c6c2a4bbd0e72dd
Lee Briggs <lee@leebriggs.co.uk>
2020-06-12 14:00:54 -0700

    update test layout

---
+++ examples/examples_go_test.go
@@ -0,0 +1,46 @@
+// Copyright 2016-2018, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// +build go all
+
+package examples
+
+func TestNginxGo(t *testing.T) {
+	cwd, err := os.Getwd()
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+
+	opts := base.With(integration.ProgramTestOptions{
+		Dependencies: []string{
+			"github.com/pulumi/pulumi-docker/sdk/v2",
+		},
+		Dir: path.Join(cwd, "nginx-go"),
+	})
+	integration.ProgramTest(t, &opts)
+}
+
+func TestDockerfileGo(t *testing.T) {
+	cwd, err := os.Getwd()
+	if !assert.NoError(t, err) {
+		t.FailNow()
+	}
+
+	opts := base.With(integration.ProgramTestOptions{
+		Dependencies: []string{
+			"github.com/pulumi/pulumi-docker/sdk/v2",
+		},
+		Dir: path.Join(cwd, "dockerfile-go"),
+	})
+	integration.ProgramTest(t, &opts)
+}
--- examples/go.mod
+++ examples/go.mod
@@ -5,6 +5,7 @@ go 1.14
 require (
 	github.com/onsi/ginkgo v1.12.0 // indirect
 	github.com/onsi/gomega v1.9.0 // indirect
+	github.com/pulumi/pulumi/pkg v1.14.1 // indirect
 	github.com/pulumi/pulumi/pkg/v2 v2.0.0
 	github.com/stretchr/testify v1.5.1
 	gopkg.in/airbrake/gobrake.v2 v2.0.9 // indirect
