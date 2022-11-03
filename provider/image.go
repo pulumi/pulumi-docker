75a7f1171a14cf9201263cc2e9a250a114867945
Guinevere Saenger <guinevere@pulumi.com>
2022-11-03 15:39:41 -0700

    Implement build and push functionality in dockerbuild function

---
+++ provider/image.go
@@ -0,0 +1,122 @@
+package provider
+
+import (
+	"bufio"
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+	structpb "github.com/golang/protobuf/ptypes/struct"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource"
+	"github.com/pulumi/pulumi/sdk/v3/go/common/resource/plugin"
+)
+
+func (p *dockerNativeProvider) dockerBuild(ctx context.Context,
+	urn resource.URN,
+	props *structpb.Struct) (*structpb.Struct, error) {
+	fmt.Println("resource URN", urn)
+
+	fmt.Println("getting inputs...")
+
+	inputs, err := plugin.UnmarshalProperties(props, plugin.MarshalOptions{KeepUnknowns: true, SkipNulls: true})
+	if err != nil {
+		return nil, err
+	}
+	//panic(fmt.Sprintf("here are the inputs: %v", inputs))
+
+	dockerfile := inputs["dockerfile"].StringValue()
+	buildContext := inputs["context"].StringValue()
+	registryURL := inputs["registryURL"].StringValue()
+	imageName := inputs["name"].StringValue()
+	//tag := inputs["tag"].StringValue()
+	registry := inputs["registry"].ObjectValue()
+	username := registry["username"].StringValue()
+	password := registry["password"].StringValue()
+
+	fmt.Println("USING THE DOCKER CLIENT NOW")
+
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Println("Get list of  existing images")
+	//imgs, err := cli.ImageList(context.Background(), types.ImageListOptions{})
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//fmt.Println("List image labels")
+	//for _, img := range imgs {
+	//	fmt.Println("whole image: ", img.RepoTags) // RepoTags gives a list of tags that correspond to the REPOSITORY and TAGS headers of the "docker images" output
+	//}
+
+	fmt.Println("now let's BUILD an image")
+	imageTag := username + "/" + imageName
+
+	// make the build context
+	tar, err := archive.TarWithOptions(buildContext, &archive.TarOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	// make the build options
+	opts := types.ImageBuildOptions{
+		Dockerfile: dockerfile,
+		Tags:       []string{imageTag},
+		Remove:     true,
+	}
+
+	imgBuildResp, err := cli.ImageBuild(context.Background(), tar, opts)
+	if err != nil {
+		panic(err)
+	}
+	// Print build logs to terminal
+	scanner := bufio.NewScanner(imgBuildResp.Body)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	fmt.Println("see if we can push to the registry")
+
+	// Quick and dirty auth; we can also preconfigure the client itself I believe
+
+	var authConfig = types.AuthConfig{
+		Username:      username,
+		Password:      password,
+		ServerAddress: registryURL,
+	}
+
+	authConfigBytes, _ := json.Marshal(authConfig)
+	authConfigEncoded := base64.URLEncoding.EncodeToString(authConfigBytes)
+
+	pushOpts := types.ImagePushOptions{RegistryAuth: authConfigEncoded}
+
+	pushOutput, err := cli.ImagePush(context.Background(), imageTag, pushOpts)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pushOutput.Close()
+
+	// Print push logs to terminal
+	pushScanner := bufio.NewScanner(pushOutput)
+	for pushScanner.Scan() {
+		fmt.Println(pushScanner.Text())
+	}
+
+	outputs := map[string]interface{}{
+		"dockerfile":  dockerfile,
+		"context":     buildContext,
+		"name":        imageTag,
+		"registryURL": registryURL,
+	}
+	return plugin.MarshalProperties(
+		resource.NewPropertyMapFromMap(outputs),
+		plugin.MarshalOptions{KeepUnknowns: true, SkipNulls: true},
+	)
+}
