78e34a3cd2626ec6b677bdce5f549586324cbfd0
Guinevere Saenger <guinevere@pulumi.com>
2023-02-14 13:23:40 -0800

    Clean up auth code and simplify

--- examples/container-registries/azure/py/__main__.py
+++ examples/container-registries/azure/py/__main__.py
@@ -1,12 +1,6 @@
 import pulumi
 import pulumi_azure as azure
-import pulumi_azuread as azuread
 from pulumi_docker import Image, RegistryArgs, DockerBuildArgs
-import pulumi_random as random
-
-# Conditionalize the auth mechanism.
-config = pulumi.Config()
-use_sp_auth = config.get_bool('useServicePrincipalAuth') or False
 
 # Create a private ACR registry.
 rg = azure.core.ResourceGroup('myrg')
@@ -15,37 +9,6 @@ registry = azure.containerservice.Registry('myregistry',
     admin_enabled=True,
     sku='Basic'
 )
-
-# # Get registry info (creds and endpoint).
-# image_name = registry.login_server.apply(lambda s: f'{s}/myapp')
-# if use_sp_auth:
-#     sp = azuread.ServicePrincipal('mysp',
-#         application_id=azuread.Application('myspapp').application_id,
-#     )
-#     sp_password = azuread.ServicePrincipalPassword('mysp-pass',
-#         service_principal_id=sp.id,
-#         value=random.RandomPassword('mypass',
-#             length=32,
-#             opts=pulumi.ResourceOptions(additional_secret_outputs=['result'])
-#         ).result,
-#         end_date_relative='8760h',
-#     )
-#     sp_auth = azure.authorization.Assignment('myauth',
-#         scope=registry.id,
-#         role_definition_name='acrpush',
-#         principal_id=sp.id,
-#     )
-#     registry_info = docker.ImageRegistry(
-#         server=registry.login_server,
-#         username=sp.application_id,
-#         password=sp_auth.id.apply(lambda _: sp_password.value),
-#         )
-# else:
-#     registry_info = docker.ImageRegistry(
-#         server=registry.login_server,
-#         username=registry.admin_username,
-#         password=registry.admin_password
-#     )
 
 # Build and publish the image.
 image = Image('my-image',
@@ -58,6 +21,5 @@ image = Image('my-image',
     )
 )
 
-# Export the resulting base name in addition to the specific version pushed.
-pulumi.export('baseImageName', image.base_image_name)
+# Export the resulting image name
 pulumi.export('imageName', image.image_name)
--- examples/container-registries/azure/ts/index.ts
+++ examples/container-registries/azure/ts/index.ts
@@ -1,12 +1,6 @@
 import * as azure from "@pulumi/azure";
-import * as azuread from "@pulumi/azuread";
 import * as docker from "@pulumi/docker";
 import * as pulumi from "@pulumi/pulumi";
-import * as random from "@pulumi/random";
-
-// Conditionalize the auth mechanism.
-const config = new pulumi.Config();
-const useServicePrincipalAuth = !!config.getBoolean("useServicePrincipalAuth");
 
 // Create a private ACR registry.
 const rg = new azure.core.ResourceGroup("myrg")
@@ -16,37 +10,7 @@ const registry = new azure.containerservice.Registry("myregistry", {
     sku: "Basic",
 });
 
-// Get registry info (creds and endpoint).
 const imageName = pulumi.interpolate`${registry.loginServer}/myapp`;
-
-// if (useServicePrincipalAuth) {
-//     const sp = new azuread.ServicePrincipal("mysp", {
-//         applicationId: new azuread.Application("myspapp").applicationId,
-//     });
-//     const spPassword = new azuread.ServicePrincipalPassword("mysp-pass", {
-//         servicePrincipalId: sp.id,
-//         value: new random.RandomPassword("mypass", {
-//             length: 32,
-//         }, { additionalSecretOutputs: [ "result" ] }).result,
-//         endDateRelative: "8760h",
-//     });
-//     const spAuth = new azure.authorization.Assignment("myauth", {
-//         scope: registry.id,
-//         roleDefinitionName: "acrpush",
-//         principalId: sp.id,
-//     });
-//     registryInfo = {
-//         server: registry.loginServer,
-//         username: sp.applicationId,
-//         password:  spAuth.id.apply(_ => spPassword.value),
-//     };
-// } else {
-//     registryInfo = {
-//         server: registry.loginServer,
-//         username: registry.adminUsername,
-//         password: registry.adminPassword,
-//     };
-// }
 
 // Build and publish the image using the auth information.
 const image = new docker.Image("my-image", {
@@ -61,6 +25,5 @@ const image = new docker.Image("my-image", {
     }
 });
 
-// Export the resuling base name in addition to the specific version pushed.
-export const baseImageName = image.baseImageName;
+// Export the resulting image name
 export const fullImageName = image.imageName;
