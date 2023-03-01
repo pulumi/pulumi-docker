566bb156e4dd56e0af4503950d43c51f6db3fe37
Guinevere Saenger <guinevere@pulumi.com>
2023-03-01 11:30:43 -0800

    Update Go

--- examples/container-registries/docker/go/main.go
+++ examples/container-registries/docker/go/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"github.com/pulumi/pulumi-docker/sdk/v2/go/docker"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v2/go/pulumi/config"
+	"github.com/pulumi/pulumi-docker/sdk/v4/go/docker"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 func main() {
@@ -12,19 +12,23 @@ func main() {
 		cfg := config.New(ctx, "")
 		username := cfg.Require("dockerUsername")
 		password := cfg.RequireSecret("dockerPassword")
+		server := "docker.io"
 
 		// Populate the registry info (creds and endpoint).
-		imageName := pulumi.String(username + "/myapp")
-		registryInfo := docker.ImageRegistryArgs{
-			Server:   pulumi.String("docker.io"),
-			Username: pulumi.String(username),
-			Password: password.(pulumi.StringOutput),
-		}
+		imageName := server + username + "/myapp"
+
+		registryInfo := password.ApplyT(func(pw string) (docker.Registry, error) {
+			return docker.Registry{
+				Server:   &server,
+				Username: &username,
+				Password: &pw,
+			}, nil
+		}).(docker.RegistryOutput)
 
 		// Build and publish the app image.
 		image, err := docker.NewImage(ctx, "my-image", &docker.ImageArgs{
 			Build:     &docker.DockerBuildArgs{Context: pulumi.String("app")},
-			ImageName: imageName,
+			ImageName: pulumi.String(imageName),
 			Registry:  registryInfo,
 		})
 		if err != nil {
