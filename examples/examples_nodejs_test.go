53a73ac72ae29f94e9a82947e0f054df4a0cd4de
Guinevere Saenger <guinevere@pulumi.com>
2023-08-11 13:16:04 +0200

    Add integration test for SSH remote connections
        Uses a Digitalocean Droplet (Docker/Ubuntu) to provide a remote Docker
        host and verifies that it can successfully create a RemoteImage and run
        a Container.
    
        We also add a waiting period for the SSH connection to become available,
        as we're encountering an intermittent Connection Refused error on
        running `up` and `destroy in quick succession.
        See https://github.com/kreuzwerker/terraform-provider-docker/issues/262
        for a likely cause.

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -22,6 +22,7 @@ import (
 	"os"
 	"path"
 	"testing"
+	"time"
 
 	"github.com/pulumi/pulumi/pkg/v3/testing/integration"
 	"github.com/stretchr/testify/assert"
@@ -193,6 +194,32 @@ func TestSecretsInExplicitProviderNode(t *testing.T) {
 		SkipRefresh:            true,
 		ExtraRuntimeValidation: check,
 	})
+	integration.ProgramTest(t, &test)
+}
+
+func TestSSHConnNode(t *testing.T) {
+	token := os.Getenv("DIGITALOCEAN_TOKEN")
+	if token == "" {
+		t.Skipf("Skipping test due to missing DIGITALOCEAN_TOKEN environment variable")
+	}
+	test := getJsOptions(t).
+		With(integration.ProgramTestOptions{
+			Dir: path.Join(getCwd(t), "test-ssh-conn", "ts"),
+			ExtraRuntimeValidation: func(t *testing.T, stack integration.RuntimeValidationStackInfo) {
+				ipOutput, ok := stack.Outputs["ipOutput"]
+				assert.True(t, ok)
+				assert.NotEmpty(t, ipOutput)
+				// Due to a bug in the docker client, we add sleep here so our SSH connection does not get refused.
+				time.Sleep(20 * time.Second)
+			},
+			SkipRefresh:      true,
+			Quick:            true,
+			RetryFailedSteps: true,
+			Config: map[string]string{
+				"digitalocean:token": token,
+			},
+			Verbose: true,
+		})
 	integration.ProgramTest(t, &test)
 }
 
