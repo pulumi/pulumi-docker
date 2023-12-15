7d7b1c219c7b737eedf2b910478a78aab54f6072
Bryce Lampe <bryce@pulumi.com>
2023-12-15 10:32:02 -0800

    Generate schema from inferred resource

--- sdk/dotnet/Buildx/Image.cs
+++ sdk/dotnet/Buildx/Image.cs
@@ -10,22 +10,58 @@ using Pulumi.Serialization;
 namespace Pulumi.Docker.Buildx
 {
     /// <summary>
-    /// Builds a Docker image with buildkit.
+    /// A Docker image built using Buildkit
     /// </summary>
     [DockerResourceType("docker:buildx/image:Image")]
     public partial class Image : global::Pulumi.CustomResource
     {
         /// <summary>
-        /// Contexts to use while building the image. If omitted, an empty context is used. If more than one value is specified, they should be of the form "[name]=[value]"
+        /// The image's architecture
+        /// </summary>
+        [Output("architecture")]
+        public Output<string?> Architecture { get; private set; } = null!;
+
+        /// <summary>
+        /// Contexts to use while building the image. If omitted, an empty context is used. If more than one value is specified, they should be of the form "name=value"
         /// </summary>
         [Output("context")]
         public Output<ImmutableArray<string>> Context { get; private set; } = null!;
+
+        /// <summary>
+        /// Name and optionally a tag (format: "name:tag"). If outputting to a registry, the name should include the fully qualified registry address.
+        /// </summary>
+        [Output("exports")]
+        public Output<ImmutableArray<string>> Exports { get; private set; } = null!;
 
         /// <summary>
         /// Name of the Dockerfile to use (default: "$PATH/Dockerfile").
         /// </summary>
         [Output("file")]
         public Output<string?> File { get; private set; } = null!;
+
+        /// <summary>
+        /// The image's operating system
+        /// </summary>
+        [Output("os")]
+        public Output<string?> Os { get; private set; } = null!;
+
+        /// <summary>
+        /// Registry digests
+        /// </summary>
+        [Output("repoDigests")]
+        public Output<ImmutableArray<string>> RepoDigests { get; private set; } = null!;
+
+        /// <summary>
+        /// Registry tags
+        /// </summary>
+        [Output("repoTags")]
+        public Output<ImmutableArray<string>> RepoTags { get; private set; } = null!;
+
+        /// <summary>
+        /// Size of the image in bytes
+        /// </summary>
+        [Output("size")]
+        public Output<int?> Size { get; private set; } = null!;
 
         /// <summary>
         /// Name and optionally a tag (format: "name:tag"). If outputting to a registry, the name should include the fully qualified registry address.
@@ -82,12 +118,24 @@ namespace Pulumi.Docker.Buildx
         private InputList<string>? _context;
 
         /// <summary>
-        /// Contexts to use while building the image. If omitted, an empty context is used. If more than one value is specified, they should be of the form "[name]=[value]"
+        /// Contexts to use while building the image. If omitted, an empty context is used. If more than one value is specified, they should be of the form "name=value"
         /// </summary>
         public InputList<string> Context
         {
             get => _context ?? (_context = new InputList<string>());
             set => _context = value;
+        }
+
+        [Input("exports")]
+        private InputList<string>? _exports;
+
+        /// <summary>
+        /// Name and optionally a tag (format: "name:tag"). If outputting to a registry, the name should include the fully qualified registry address.
+        /// </summary>
+        public InputList<string> Exports
+        {
+            get => _exports ?? (_exports = new InputList<string>());
+            set => _exports = value;
         }
 
         /// <summary>
@@ -110,6 +158,7 @@ namespace Pulumi.Docker.Buildx
 
         public ImageArgs()
         {
+            File = "Dockerfile";
         }
         public static new ImageArgs Empty => new ImageArgs();
     }
