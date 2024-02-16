2e5967cd5f34e1cad9101e7470b127d6944ba9f4
Bryce Lampe <bryce@pulumi.com>
2024-02-16 12:21:06 -0800

    Enable yaml tests in CI

--- examples/examples_yaml_test.go
+++ examples/examples_yaml_test.go
@@ -11,8 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//go:build go || all
-// +build go all
+//go:build yaml || all
+// +build yaml all
 
 package examples
 
