500d923af908c0db566e0b55e3569c4c228eee59
Ramon Quitales <ramon@pulumi.com>
2025-04-09 17:04:44 -0700

    chore: update ts test code to use artifact registry

--- examples/gcp-artifact-registry/ts/index.ts
+++ examples/gcp-artifact-registry/ts/index.ts
@@ -1,13 +1,30 @@
 import * as gcp from "@pulumi/gcp";
+import * as pulumi from "@pulumi/pulumi";
+import * as random from "@pulumi/random";
 import * as docker from "@pulumi/docker";
 
-// Create a private GCR registry.
-const registry = new gcp.container.Registry("my-registry");
-const registryUrl = registry.id.apply(_ =>
-    gcp.container.getRegistryRepository().then(reg => reg.repositoryUrl));
+// Create a random suffix for the repository name.
+let randomSuffix = new random.RandomString("random-suffix", {
+    length: 6,
+    special: false,
+    upper: false
+});
+let repoName = pulumi.concat(`docker-test-repo-`, randomSuffix.result);
+
+// Create a private GCP artifact registry.
+const registry = new gcp.artifactregistry.Repository("my-registry", {
+    format: "DOCKER",
+    repositoryId: repoName,
+    location: "us-central1", // change to your desired region
+    dockerConfig: {
+        immutableTags: false,
+    },
+});
+
+const registryUrl = pulumi.interpolate `${registry.location}-docker.pkg.dev/${registry.project}/${registry.repositoryId}`;
 
 // Get registry info (creds and endpoint).
-const imageName = registryUrl.apply(url => `${url}/myapp`);
+const imageName = pulumi.interpolate `${registryUrl}/myapp`;
 
 // Build and publish the image.
 const image = new docker.Image("my-image", {
