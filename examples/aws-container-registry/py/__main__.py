26e0f8cc8d8ce84b8369e9c1c0d0bc6fa2b9c9e3
guineveresaenger <guinevere@pulumi.com>
2025-07-23 13:03:09 -0700

    update Python

--- examples/aws-container-registry/py/__main__.py
+++ examples/aws-container-registry/py/__main__.py
@@ -13,14 +13,11 @@ repo = aws.ecr.Repository(
 # Get registry info (creds and endpoint) so we can build/publish to it.
 def getRegistryInfo(rid):
     creds = aws.ecr.get_credentials(registry_id=rid)
-    decoded = base64.b64decode(creds.authorization_token).decode()
-    parts = decoded.split(':')
-    if len(parts) != 2:
-        raise Exception("Invalid credentials")
+
     return RegistryArgs(
         server=creds.proxy_endpoint,
-        username=parts[0],
-        password=parts[1],
+        username=creds.user_name,
+        password=creds.password,
     )
 
 
