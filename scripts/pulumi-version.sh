4bf8bf843ed48fad4609ff8f38456c1c247a34e6
corymhall <43035978+corymhall@users.noreply.github.com>
2025-09-19 13:02:35 -0400

    try something

--- scripts/pulumi-version.sh
+++ scripts/pulumi-version.sh
@@ -1,3 +1,4 @@
-export PULUMI_VERSION=$(cd provider && go list -f "{{slice .Version 1}}" -m github.com/pulumi/pulumi/pkg/v3)
-# echo "PULUMI_VERSION=$version"
-# export PULUMI_VERSION=$version
+# version=$(cd provider && go list -f "{{slice .Version 1}}" -m github.com/pulumi/pulumi/pkg/v3)
+version="3.190.0"
+echo "PULUMI_VERSION=$version"
+export PULUMI_VERSION=$version
