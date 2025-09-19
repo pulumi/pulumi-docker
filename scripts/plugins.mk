2e6e2ba92d853274200eac10010ead2ab6722f4c
corymhall <43035978+corymhall@users.noreply.github.com>
2025-09-19 14:06:00 -0400

    fixing

--- scripts/plugins.mk
+++ scripts/plugins.mk
@@ -2,7 +2,7 @@
 install_plugins: .make/install_plugins
 .make/install_plugins: export PULUMI_HOME := $(WORKING_DIR)/.pulumi
 # .make/install_plugins: export PATH := $(WORKING_DIR)/.pulumi/bin:$(PATH)
-.make/install_plugins:# .pulumi/bin/pulumi
+.make/install_plugins::
 	pulumi plugin install converter terraform 1.0.16
 	pulumi plugin install resource aws 6.8.0
 	@touch $@
