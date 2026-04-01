54d2fc5beb57879d77850e4c880179023233c012
guineveresaenger <guinevere@pulumi.com>
2026-04-01 10:27:35 -0700

    Revert "Fix test-ssh-conn: update DigitalOcean droplet size"
    This reverts commit 55e4e7e9b630892109b516e77ff3683f4c8248f1.

--- examples/test-ssh-conn/ts/index.ts
+++ examples/test-ssh-conn/ts/index.ts
@@ -5,7 +5,7 @@ import * as digitalocean from "@pulumi/digitalocean";
 const remoteHost = new digitalocean.Droplet("docker-host-test", {
     image: "docker-20-04",
     region: "fra1",
-    size: "s-2vcpu-4gb",
+    size: "c-8",
     sshKeys: ["cb:dd:70:4d:49:2f:86:eb:fd:bb:e4:8b:04:fc:b0:cb"],
 })
 
